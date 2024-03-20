73d319bd9641875fde9468d31222019561312223
Jorge Brandon Chandomi Esponda <Sr.conejo.uwu@gmail.com>
2024-03-20 14:12:01 -0600

    programar controlador (#3)

--- src/main/java/com/jbunce/holamundofx/HelloController.java
+++ src/main/java/com/jbunce/holamundofx/HelloController.java
@@ -2,13 +2,19 @@ package com.jbunce.holamundofx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    private TextField numero1;
+    private TextField numero2;
+
+    private Label result;
+
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        result.setText("Resultado: " + (Integer.parseInt(numero1.getText()) + Integer.parseInt(numero2.getText())));
     }
 }
